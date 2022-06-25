efc4a44159a27946f364ac5dd08a8dd5442b5f0d
GGTHKK <babagatom1@gmail.com>
2022-06-26 00:35:25 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div style="height: 360px; position: relative">
+        <n-layout position="absolute">
+            <n-layout-header style="height: 64px; padding: 24px" bordered> Yiheyuan Road </n-layout-header>
+            <n-layout has-sider position="absolute" style="top: 64px; bottom: 64px">
+                <n-layout-sider bordered content-style="padding: 24px;"> Handian Bridge </n-layout-sider>
+                <n-layout content-style="padding: 24px;">
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                    <n-h2>Pingshan Road</n-h2>
+                </n-layout>
+            </n-layout>
+            <n-layout-footer bordered position="absolute" style="height: 64px; padding: 24px"> 城府路 </n-layout-footer>
+        </n-layout>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
     name: 'HomeView',
-    components: {
-        HelloWorld,
-    },
 })
 </script>
