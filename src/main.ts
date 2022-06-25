6529d3cb89ded02407d51ee792e3528ab62550b5
GGTHKK <babagatom1@gmail.com>
2022-06-25 21:45:07 +0700

    config and add naveui success

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '/src/assets/index.css'
+import naive from 'naive-ui'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(naive).mount('#app')
