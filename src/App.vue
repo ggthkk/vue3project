02275a6dd389d4158dc07c28d6e258844a61b556
ggthkk <babagatom1@gmail.com>
2022-07-04 13:22:11 +0700

    update map

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-    <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <div style="height: 75vh; width: 50vw">
+        <LMap v-model="zoom" :center="[47.41322, -1.219482]" @move="log('move')">
+            <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></LTileLayer>
+            <LControlLayers />
+
+            <LMarker :lat-lng="[13.668, 100.503]" draggable @moveend="log('moveend')">
+                <LPopup> lolddd </LPopup>
+            </LMarker>
+        </LMap>
+        <button @click="changeIcon">New kitten icon</button>
+    </div>
 </template>
+<script>
+import { LMap, LTileLayer, LMarker, LControlLayers, LPopup } from '@vue-leaflet/vue-leaflet'
+import 'leaflet/dist/leaflet.css'
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+export default {
+    components: {
+        LMap,
+        LTileLayer,
+        LMarker,
+        LControlLayers,
+        LPopup,
+    },
+    data() {
+        return {
+            zoom: 2,
+            iconWidth: 25,
+            iconHeight: 40,
+        }
+    },
+    computed: {
+        iconUrl() {
+            return `https://placekitten.com/${this.iconWidth}/${this.iconHeight}`
+        },
+        iconSize() {
+            return [this.iconWidth, this.iconHeight]
+        },
+    },
+    methods: {
+        log(a) {
+            console.log(a)
+        },
+        changeIcon() {
+            this.iconWidth += 2
+            if (this.iconWidth > this.iconHeight) {
+                this.iconWidth = Math.floor(this.iconHeight / 2)
+            }
+        },
+    },
 }
-
-nav {
-    padding: 30px;
-}
-
-nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-    color: #42b983;
-}
-</style>
+</script>
